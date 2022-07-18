b79adf4dca0c4f11c1081319292775d2327b16a1
zhanpengchan <407065462@qq.com>
2022-07-18 12:01:55 +0800

    刷新token

--- src/main/java/com/manong/controller/SysUserController.java
+++ src/main/java/com/manong/controller/SysUserController.java
@@ -1,10 +1,19 @@
 package com.manong.controller;
 
+import com.manong.config.redis.RedisService;
+import com.manong.utils.JwtUtils;
 import com.manong.utils.Result;
+import com.manong.vo.TokenVo;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -17,6 +26,10 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/api/sysUser")
 public class SysUserController {
+    @Resource
+    private JwtUtils jwtUtils;
+    @Resource
+    private RedisService redisService;
     /**
      * 刷新token
      * @param request
@@ -25,7 +38,37 @@ public class SysUserController {
     @PostMapping("/refreshToken")
     public Result refreshToken(HttpServletRequest request){
 //        从headers获取token
-        String token = request.getRequestURI();
+        String token = request.getHeader("token");
+        if(!ObjectUtils.isEmpty(token)){
+//            从请求参数获取token
+            token = request.getParameter("token");
+        }
+//        从Spring security上下文获取用户信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        获取用户身份信息
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//        定义变量保存新的token信息
+        String newToken = "";
+//        验证提交过来的token 信息是否合法
+        if (jwtUtils.validateToken(token,userDetails)) {
+//            生成新的token
+            newToken = jwtUtils.refreshToken(token);
+
+        }
+//            获取新的token过期时间
+        long expireTime = Jwts.parser()
+                .setSigningKey(jwtUtils.getSecret())
+                .parseClaimsJws(newToken.replace("jwt_", ""))
+                .getBody().getExpiration().getTime();
+//            清除旧的token
+        String oldTokenKey = "token_" + token;
+        redisService.del(oldTokenKey);
+//            将新的token保存到redis缓存中
+        String newTokenKey = "token_" + newToken;
+        redisService.set(newTokenKey,newToken,jwtUtils.getExpiration()/1000);
+//            创建tokenVo对象
+        TokenVo tokenVo = new TokenVo(expireTime,newToken);
+        return Result.ok(tokenVo).message("token刷新成功");
     }
 
 }
